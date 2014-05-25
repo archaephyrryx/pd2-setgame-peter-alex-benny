8d91c54d967db29706e71c96e3115980f35cdf72
Peter Duchovni <caufeminecraft+github@gmail.com>
2014-05-25 18:36:07 -0400

    Deal and Shuffle fixed
    Added deterministic n-choose-m subset function in Shuffle.hs
    Deal.hs working

--- Deal.hs
+++ Deal.hs
@@ -4,14 +4,8 @@ import SetCards
 import Shuffle
 import System.Random
 
-
-
-deal :: Int -> (Int -> [Card])
-deal n = \x -> map toEnum (take n (shuffle 81 x [0..80]))
-
 main = do
     g <- newStdGen
-    let x = (fst (randomR (1, (!) 81) g )) - 1
-    let n = deal 12 x
-    print n
-
+    let x = fst $ randomR (0, (choose 81 12) - 1) g
+    let n = deal 12 81 x [0..80]
+    print ((map toEnum n) :: [Card])
--- Shuffle.hs
+++ Shuffle.hs
@@ -3,6 +3,9 @@ module Shuffle where
 (!) :: Int -> Int
 (!) n = product [1..n]
 
+choose :: Int -> Int -> Int
+choose a b = (product [a-b+1..a])`div`(product [1..b])
+
 omit :: Int -> ([a] -> [a])
 omit 0 = tail
 omit n = (\(x:xs) -> x:(omit (n-1) xs))
@@ -14,3 +17,16 @@ shuffle n i | q == 0  = (\(x:xs) -> x:(shuffle (n-1) r xs))
 	    where
 		 q = i `div` ((!) (n-1)) 
 		 r = i `rem` ((!) (n-1)) 
+
+
+deal :: Int -> Int -> Int -> ([a] -> [a])
+deal m n i | m > n = (\_ -> [])
+           | m == 0 = (\_ -> [])
+	   | m == n = shuffle n i
+	   | i < q = (\(x:xs) -> x:(deal (m-1) (n-1) i xs))
+	   | otherwise = (\(_:xs) -> (deal m (n-1) r xs))
+	   where
+                q = (choose (n-1) (m-1))
+		r = i - q
+
+
